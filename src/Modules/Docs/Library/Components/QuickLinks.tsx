e6ff5ed6391ee281cd2e1d6d42967ae909fbc588
Kodemon <hi@kodemon.net>
2023-05-15 23:11:52 +0900

    chore(ipfs): add some initial details on IPFS

--- src/Modules/Docs/Library/Components/QuickLinks.tsx
+++ src/Modules/Docs/Library/Components/QuickLinks.tsx
@@ -10,7 +10,7 @@ export function QuickLink({ title, description, href }: any) {
       <div class="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
       <div class="relative overflow-hidden rounded-xl p-6">
         <h2 class="font-display text-base text-slate-900 dark:text-white">
-          <Link href={href}>
+          <Link href={href} target="_blank">
             <span class="absolute -inset-px rounded-xl" />
             {title}
           </Link>
