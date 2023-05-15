a9831edb6450ee528791b5a812f227bca5109d85
Kodemon <hi@kodemon.net>
2023-05-15 15:42:16 +0900

    chore(response): server response test

--- server/serverless.ts
+++ server/serverless.ts
@@ -1,32 +1,23 @@
-import { readFileSync } from "node:fs";
-import { resolve } from "node:path";
+// import { readFileSync } from "node:fs";
+// import { resolve } from "node:path";
 
 import express from "express";
 
 import { crawlers } from "./Crawlers";
-import { renderPage } from "./Render";
+// import { renderPage } from "./Render";
 
 const app = express();
 
-const index = readFileSync(resolve(__dirname, "..", "dist", "index.html"), "utf-8");
+// const index = readFileSync(resolve(__dirname, "..", "dist", "index.html"), "utf-8");
 
-app.use(async (req, res, next) => {
+app.use("*", async (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+
   const userAgent = (req.query["user-agent"] ?? req.headers["user-agent"]) as string | undefined;
   const isCrawler = userAgent !== undefined && crawlers.some((crawler) => userAgent.includes(crawler)) === true;
 
-  console.log({ isCrawler, userAgent });
-
-  if (isCrawler === true) {
-    res.send(await renderPage(req));
-  } else {
-    next();
-  }
-});
-
-app.all("*", async (_req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(index);
+  res.end(JSON.stringify({ userAgent, isCrawler }));
 });
 
 export default app;
