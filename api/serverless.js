a339beeaf6918feb64ea9d7fe7d3d15c62d6cafe
Kodemon <hi@kodemon.net>
2023-05-16 00:43:17 +0900

    chore(seo): add prerender middleware

--- api/serverless.js
+++ api/serverless.js
@@ -1,21 +1,16 @@
 const { readFileSync } = require("node:fs");
 const { resolve } = require("node:path");
 
-const { crawlers } = require("./Crawlers");
-const { renderPage } = require("./Render");
-
 const app = require("express")();
 
 const index = readFileSync(resolve(__dirname, "..", "dist", "index.html"), "utf-8");
 
-app.use("*", async (req, res) => {
+app.use(require('prerender-node').set('prerenderToken', 'xYeGNjAfNaqMmbkcKohK'));
+
+app.use("*", async (_, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-
-  const userAgent = req.query["user-agent"] ?? req.headers["user-agent"];
-  const isCrawler = userAgent !== undefined && crawlers.some((crawler) => userAgent.includes(crawler)) === true;
-
-  res.end(isCrawler === true ? await renderPage(req) : index);
+  res.end(index);
 });
 
 module.exports = app;
