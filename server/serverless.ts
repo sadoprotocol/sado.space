f8cef3ef45d2211d68c4364692f26c7661ef53f2
Kodemon <hi@kodemon.net>
2023-05-15 15:35:42 +0900

    chore(user-agent): update user agent handling

--- server/serverless.ts
+++ server/serverless.ts
@@ -10,14 +10,23 @@ const app = express();
 
 const index = readFileSync(resolve(__dirname, "..", "dist", "index.html"), "utf-8");
 
-app.all("*", async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-
+app.use(async (req, res, next) => {
   const userAgent = (req.query["user-agent"] ?? req.headers["user-agent"]) as string | undefined;
   const isCrawler = userAgent !== undefined && crawlers.some((crawler) => userAgent.includes(crawler)) === true;
 
-  res.end(isCrawler === true ? await renderPage(req) : index);
+  console.log({ isCrawler, userAgent });
+
+  if (isCrawler === true) {
+    res.send(await renderPage(req));
+  } else {
+    next();
+  }
+});
+
+app.all("*", async (_req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(index);
 });
 
 export default app;
