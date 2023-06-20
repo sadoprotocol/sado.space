fa4ab1cae69c75abfaba9b15d17d2b190c5f7523
Kodemon <hi@kodemon.net>
2023-06-21 01:13:33 +0900

    chore(docs): fix mobile header scroll bug

--- src/Modules/Docs/Components/Header.tsx
+++ src/Modules/Docs/Components/Header.tsx
@@ -8,11 +8,9 @@ import { MobileNavigation } from "./MobileNavigation";
 export const Header = HeaderController.view(({ state }) => {
   return (
     <header
+      class="sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8"
       classList={{
-        "sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8":
-          true,
-        "dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75":
-          state.isScrolled,
+        "dark:bg-slate-900/95 dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75": state.isScrolled,
         "dark:bg-transparent": !state.isScrolled
       }}
     >
