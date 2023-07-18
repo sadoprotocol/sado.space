88673349fc9cf6cdc1eca555213e470a97d96ae6
msmalley <mark@smalley.my>
2023-07-18 12:33:49 +0800

    Update Navigation.ts

--- src/Navigation.ts
+++ src/Navigation.ts
@@ -10,7 +10,7 @@ export const navigation: Navigation = {
       title: "Blog Articles",
       links: [
         { title: "Tapping into the magic of taproot!", href: "/blog/taproot-recoverable-commits", file: "/blog/taproot.md" },
-        { title: "Why worry about being ordinal-aware?", href: "/blog/ordinal-aware", file: "/blog/aware.md" }
+        { title: "Be aware of what is ordinal-aware!", href: "/blog/ordinal-aware", file: "/blog/aware.md" }
       ]
     }
   ],
